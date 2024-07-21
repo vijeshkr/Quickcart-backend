8f4f57f0e74c613573859481e037491942394d27
Vijesh KR <vijeshkr17@gmail.com>
2024-07-22 03:17:20 +0530

    index file updated

--- index.js
+++ index.js
@@ -30,16 +30,8 @@ app.use(cors({
     credentials: true,
     methods : ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
 }));
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
 app.use(cookieParser());
+app.use(express.json());
 
 // Serve uploaded files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
