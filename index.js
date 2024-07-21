528839b0779815c326c3e69495f8925a0f3625e3
Vijesh KR <vijeshkr17@gmail.com>
2024-07-22 02:22:23 +0530

    index file updated

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ const storage = multer.diskStorage({
 // Middlewares
 app.use(cookieParser());
 app.use(cors({
-    // origin: process.env.FRONTEND_URL,
-    origin : '*',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods : ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
 }));
