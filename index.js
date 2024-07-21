ada4a121de91eaa9879e1f8972c39cd87ccc3cb8
Vijesh KR <vijeshkr17@gmail.com>
2024-07-22 03:06:36 +0530

    index file updated

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
